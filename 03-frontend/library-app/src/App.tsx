5d5080438c0c3a62f1fbca4a5984910602a60d2b
lewisaudeone <luiz.carlos@saudeone.net>
2023-09-30 14:16:07 -0300

    Section 14: Library Project - React Routing

--- 03-frontend/library-app/src/App.tsx
+++ 03-frontend/library-app/src/App.tsx
@@ -3,13 +3,27 @@ import "./App.css";
 import { Navbar } from "./layout/NavBarAndFooter/NavBar";
 import { Footer } from "./layout/NavBarAndFooter/Footer";
 import { SearchBooksPage } from "./layout/SearchBooksPage/SearchBooksPage";
+import { HomePage } from "./layout/HomePage/HomePage";
+import { Redirect, Route, Switch } from "react-router-dom";
 
 export const App = () => {
   return (
-    <div>
+    <div className="d-flex flex-column min-vh-100">
       <Navbar />
-      {/* <HomePage /> */}
-      <SearchBooksPage />
+      <div className="flex-grow-1">
+        <Switch>
+          <Route path="/" exact>
+            <Redirect to="/home" />
+          </Route>
+          <Route path="/home">
+            <HomePage />
+          </Route>
+          <Route path="/search">
+            <SearchBooksPage />
+          </Route>
+        </Switch>
+      </div>
+
       <Footer />
     </div>
   );

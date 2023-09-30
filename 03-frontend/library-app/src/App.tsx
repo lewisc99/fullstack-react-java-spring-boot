844d089ac661e5db2e484e2c8d6ff103e210c1f4
lewisaudeone <luiz.carlos@saudeone.net>
2023-09-30 18:05:29 -0300

    Section 15: Library Project - React - Book Checkout

--- 03-frontend/library-app/src/App.tsx
+++ 03-frontend/library-app/src/App.tsx
@@ -5,6 +5,7 @@ import { Footer } from "./layout/NavBarAndFooter/Footer";
 import { SearchBooksPage } from "./layout/SearchBooksPage/SearchBooksPage";
 import { HomePage } from "./layout/HomePage/HomePage";
 import { Redirect, Route, Switch } from "react-router-dom";
+import { BookCheckoutPage } from "./layout/BookCheckoutPage/BookCheckoutPage";
 
 export const App = () => {
   return (
@@ -20,6 +21,9 @@ export const App = () => {
           </Route>
           <Route path="/search">
             <SearchBooksPage />
+          </Route>
+          <Route path="/checkout/:bookId">
+            <BookCheckoutPage />
           </Route>
         </Switch>
       </div>
